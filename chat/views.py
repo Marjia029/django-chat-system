e70a95363c1809ee47da6a9b07f3a6eed3181d28
Marjia029 <marjiaafroj2@gmail.com>
2026-02-10 13:17:16 +0600

    #Fix - filters added

--- chat/views.py
+++ chat/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.parsers import MultiPartParser, FormParser
 from django.db.models import Q, Max
 from django.contrib.auth import get_user_model
 from .models import Message
