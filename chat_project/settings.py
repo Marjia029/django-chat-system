f5e0a472ca984d3836026649470d1249bb51df32
Marjia029 <marjiaafroj2@gmail.com>
2026-02-10 15:48:48 +0600

    #Fix - smtp email server configured

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -31,7 +31,12 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY' )
 DEBUG = os.getenv('DJANGO_DEBUG', 'True').lower() in ('1', 'true', 'yes')
 
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = [
+    "http://192.168.68.122:3000",
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://192.168.68.122:3000",
+]
 
 # Application definition
 
@@ -193,8 +198,14 @@ CHANNEL_LAYERS = {
 }
 
 # Email Configuration
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 MEDIA_URL = os.getenv('MEDIA_URL')
 MEDIA_ROOT = BASE_DIR / os.getenv('MEDIA_ROOT')
